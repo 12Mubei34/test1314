036f5a6b1d54e40b4a4b525e030ca1674cb3f085
13Mubei14 <1286480227@qq.com>
2021-01-30 22:29:00 +0800

    代码

---
+++ 2021 1 27/2021 1 27/test.c
@@ -0,0 +1,46 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//求两个数二进制中不同位的个数
+//#include <stdio.h>
+//int calc_diff_bit(int m, int n)
+//{
+//	int tmp = m^n;
+//	int count = 0;
+//	while (tmp)
+//	{
+//		tmp = tmp&(tmp - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//
+//
+//int main()
+//{
+//	int m, n;
+//	while (scanf("%d %d", &m, &n) == 2)
+//	{
+//		printf("%d\n", calc_diff_bit(m, n));
+//	}
+//	return 0;
+//}
+//void Printbit(int num)
+//{
+//	for (int i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d ", (num >> i) & 1);
+//	}
+//	printf("\n");
+//
+//	for (int i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d ", (num >> i) & 1);
+//	}
+//	printf("\n");
+//}
+#include<stdio.h>
+int main()
+{
+	printf("nihao\n");
+	return 0;
+}
