0d67866a94c1b85e03e57e377795cd93c26293be
13Mubei14 <1286480227@qq.com>
2021-02-17 10:06:08 +0800

    利用递归实现打印一个整数的每一位

---
+++ 2021_2_17/2021_2_17/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//利用递归实现打印一个整数的每一位
+void print(int n)
+{
+	if (n > 9)
+	{
+		print(n / 10);
+	}
+	printf("%d ", n % 10);
+}
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	print(num);
+	return 0;
+}
