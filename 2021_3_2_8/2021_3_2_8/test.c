3a116f3a1672425b7e019599fe6dacc8c92c6124
13Mubei14 <1286480227@qq.com>
2021-03-02 17:23:37 +0800

    学校作业

---
+++ 2021_3_2_8/2021_3_2_8/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d", &x);
+	if (x == 5)
+	{
+		printf("*****\n");
+	}
+	else
+	{
+		printf("#####\n");
+	}
+	return 0;
+}
